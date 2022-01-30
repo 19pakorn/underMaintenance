cb72d2c7bed3a0711b93f6b0c037b2507f91191b
19pakorn <65450790+19pakorn@users.noreply.github.com>
2022-01-30 16:58:21 +0700

    main page

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" >
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kanit">
+    <link rel="stylesheet" href="//cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"  crossorigin="anonymous">
+    <link rel='shortcut icon' type='image/x-icon' href='asset/favicon.ico' />
+    <link rel="stylesheet" href="asset/css/me.css">
+    <title>underMaintenance</title>
+
+</head>
+<body>
+
+<div class="container-fluid">
+    <!-- <div id="error"> </div> -->
+    <div class="card" id="Content">
+    <div class="text-center">
+        <img alt="" src="imange/process.png" width="40%" height="40%"></div> 
+                <div class="text-center mt-2 pt-2 text-primary"></div>
+                <div class="container text-center">
+                    <p> 
+                        <b>ขออภัย<br>
+                        <b>ไม่สามารถเปิดรับชำระค่าน้ำประปาได้ในขณะนี้<br>
+                        <b>เนื่องจากอยู่ระหว่างการปรับปรุงระบบรักษาความปลอดภัย<br>
+                    </p>
+                </div>
+</div>
+</div>
+</body>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" ></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"></script>
+<!-- <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script> -->
+<!-- <script src="//cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script> -->
+<!-- <script src="asset/js/js.js"></script> -->
+<script>
+
+
+</script>
+</html>
